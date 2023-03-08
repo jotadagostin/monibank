f63d82496419cff583f4282f9ec3baf4fb910835
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-08 11:02:17 +0100

    Update camera.js
    js code camera ready

--- js/camera.js
+++ js/camera.js
@@ -4,6 +4,7 @@ const video = document.querySelector("[data-video]");
 const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
 const canvas = document.querySelector("[data-video-canvas]");
 const mensagem = document.querySelector("[data-mensagem]");
+const botaoEnviarFoto = document.querySelector("[data-enviar]");
 let ImagemURL = "";
 
 botaoIniciarCamera.addEventListener("click", async function () {
@@ -24,4 +25,13 @@ botaoTirarFoto.addEventListener("click", function () {
   ImagemURL = canvas.toDataURL("image/jpeg");
   campoCamera.style.display = "none";
   mensagem.style.display = "block";
+});
+
+botaoEnviarFoto.addEventListener("click", () => {
+  const receberDadosExistentes = localStorage.getItem("cadastro");
+  const converteRetorno = JSON.parse(receberDadosExistentes);
+
+  converteRetorno.imagem = ImagemURL;
+  localStorage.setItem("cadastro", JSON.stringify(converteRetorno));
+  window.location.href = "./abrir-conta-form-3.html";
 });
